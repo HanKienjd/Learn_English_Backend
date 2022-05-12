7c8bbc224468b023126bc0d8246b2206f3582e2c
Dang Truong Giang <giangdt.kma@gmail.com>
2022-05-12 21:29:55 +0700

    fix forgot password api and config nodemailer

--- src/configs/mail.config.js
+++ src/configs/mail.config.js
@@ -47,7 +47,7 @@ const sendEmail = async ({ to, subject, text, html, ...rest }) => {
 };
 
 const headerHtmlMail = `<h1 style="color: #018c4c; font-size: 48px; border-bottom: solid 2px #ccc;padding-bottom: 10px">
-      Learn English<br />
+      Amonino App<br />
     </h1>`;
 const footerHtmlVerifyMail = `<h3 style="color: red">
         Chú ý: Không đưa mã này cho bất kỳ ai,
@@ -62,7 +62,7 @@ const htmlSignupAccount = (token) => {
     ${headerHtmlMail}
     <h2 style="padding: 10px 0; margin-bottom: 10px;">
         Xin chào anh (chị),<br />
-        Mã xác nhận đăng ký tài khoản cho website Learn English của bạn.<br />
+        Mã xác nhận đăng ký tài khoản cho ứng dụng Amonino của bạn.<br />
         Cảm ơn vì đã ghé thăm TTB Store <3
     </h2>
     <h3 style="background: #eee;padding: 10px;">
@@ -78,7 +78,7 @@ const htmlResetPassword = (token) => {
     ${headerHtmlMail}
     <h2 style="padding: 10px 0; margin-bottom: 10px;">
         Xin chào anh (chị),<br />
-        Dynon đã nhận được yêu cầu lấy lại mật khẩu từ bạn.<br />
+        Amonino đã nhận được yêu cầu lấy lại mật khẩu từ bạn.<br />
         Đừng lo lắng, hãy nhập mã này để khôi phục:
     </h2>
     <h1 style="background: #eee;padding: 10px;">
--- src/services/common.service.js
+++ src/services/common.service.js
@@ -1,40 +1,40 @@
-const { cloudinary } = require('../configs/cloudinary.config');
-const { MAX } = require('../constant');
-const { convertPackInfoToQueryStr } = require('../helper/word-pack.helper');
-const VerifyCodeModel = require('../models/account.model/verifyCode.model');
-const SentenceModel = require('../models/sentence.model');
-const WordModel = require('../models/word.model');
+const { cloudinary } = require('../configs/cloudinary.config')
+const { MAX } = require('../constant')
+const { convertPackInfoToQueryStr } = require('../helper/word-pack.helper')
+const VerifyCodeModel = require('../models/account.model/verifyCode.model')
+const SentenceModel = require('../models/sentence.model')
+const WordModel = require('../models/word.model')
 
 exports.uploadImage = async (imgSrc, folderName = '', config = {}) => {
   try {
     const result = await cloudinary.uploader.upload(imgSrc, {
       folder: folderName,
       ...config,
-    });
-    const { secure_url = null } = result;
-    return secure_url;
+    })
+    const { secure_url = null } = result
+    return secure_url
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 exports.isExistWord = async (word = '', type = '') => {
   try {
     if (word === '' || type === '') {
-      return false;
+      return false
     }
 
-    return await WordModel.exists({ word, type });
+    return await WordModel.exists({ word, type })
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 exports.isExistSentence = async (sentence = '') => {
-  if (sentence === '') return false;
-  const newRegex = new RegExp(sentence, 'i');
-  return await SentenceModel.exists({ sentence: newRegex });
-};
+  if (sentence === '') return false
+  const newRegex = new RegExp(sentence, 'i')
+  return await SentenceModel.exists({ sentence: newRegex })
+}
 
 exports.getWordPack = async (
   packInfo = {},
@@ -45,78 +45,81 @@ exports.getWordPack = async (
   expandQuery = null,
 ) => {
   try {
-    let query = convertPackInfoToQueryStr(packInfo);
+    let query = convertPackInfoToQueryStr(packInfo)
 
     // add expand query
     if (expandQuery && typeof expandQuery === 'object') {
-      Object.assign(query, expandQuery);
+      Object.assign(query, expandQuery)
     }
 
     const packList = await WordModel.find(query)
       .sort({ word: sortType })
       .skip(skip)
       .limit(limit)
-      .select(select);
+      .select(select)
 
-    return packList;
+    return packList
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 exports.countWordPack = async (packInfo = {}) => {
   try {
-    let query = convertPackInfoToQueryStr(packInfo);
-    return await WordModel.countDocuments(query);
+    let query = convertPackInfoToQueryStr(packInfo)
+    return await WordModel.countDocuments(query)
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 exports.saveVerifyCode = async (code = '', email = '') => {
   try {
     // delete old code
-    await VerifyCodeModel.deleteOne({ email });
+    await VerifyCodeModel.deleteOne({ email })
 
     const newItem = await VerifyCodeModel.create({
       code,
       email,
       createdDate: new Date(),
-    });
-    return newItem;
+    })
+    return newItem
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 exports.checkVerifyCode = async (code = '', email = '') => {
   try {
-    const item = await VerifyCodeModel.findOne({ email, code });
+    const item = await VerifyCodeModel.findOne({ email })
 
     if (!item) {
-      return { status: false, message: 'Hãy gửi mã để nhận mã xác thực.' };
+      return {
+        status: false,
+        message: 'Không tồn tại mã xác thực nào cho email : ' + email,
+      }
     }
 
     if (item.code !== code) {
-      return { status: false, message: 'Mã xác thực không đúng.' };
+      return { status: false, message: 'Mã xác thực không đúng.' }
     }
 
     const d = new Date().getTime(),
-      createdDate = new Date(item.createdDate).getTime();
+      createdDate = new Date(item.createdDate).getTime()
 
     if (d - createdDate > MAX.VERIFY_TIME) {
       return {
         status: false,
         message: 'Mã xác thực đã hết hiệu lực. Hãy lấy một mã khác',
-      };
+      }
     }
 
-    return { status: true, message: 'valid' };
+    return { status: true, message: 'valid' }
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 exports.removeVerifyCode = async (email = '') => {
-  await VerifyCodeModel.deleteOne({ email });
-};
+  await VerifyCodeModel.deleteOne({ email })
+}
